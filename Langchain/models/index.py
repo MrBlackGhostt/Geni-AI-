68df873daec423e180750fa3de7ed8fd4994cb12
MrBlackGhostt <hemantkumar4213@icloud.com>
2025-09-05 12:10:47 +0530

    Initial commit - cleaned history

--- Langchain/models/index.py
+++ Langchain/models/index.py
@@ -1,14 +1,17 @@
 from langchain_openai import OpenAI, chat_models
 
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, AIMessage
 
-messages = [
-    SystemMessage(content="Translate the following from English into Italian"),
-    HumanMessage(content="hi there"),
-]
+
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
-llm = OpenAI()
-response = model.invoke(messages)
-print(response.content)
+print(
+    model.invoke(
+        [
+            HumanMessage(content="Hi! I'm Bob"),
+            AIMessage(content="Hello Bob! How can I assist you today?"),
+            HumanMessage(content="What's my name?"),
+        ]
+    )
+)
